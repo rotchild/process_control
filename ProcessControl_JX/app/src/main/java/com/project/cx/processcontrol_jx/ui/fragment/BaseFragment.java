8667489bcfa29a11ee8057a7a3dbef07f01c0f2f
rotchild <2594962259@qq.com>
2018-02-06 23:57:20 +0800

    ++

--- ProcessControl_JX/app/src/main/java/com/project/cx/processcontrol_jx/ui/fragment/BaseFragment.java
+++ ProcessControl_JX/app/src/main/java/com/project/cx/processcontrol_jx/ui/fragment/BaseFragment.java
@@ -3,17 +3,30 @@ package com.project.cx.processcontrol_jx.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.project.cx.processcontrol_jx.R;
+import com.project.cx.processcontrol_jx.widget.MultiStateView;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2018/2/6 0006.
  */
 
 public abstract class BaseFragment extends Fragment {
+    @BindView(R.id.multi_state_view)
+    MultiStateView multiStateView;
+    @BindView(R.id.refreshlayout)
+    RefreshLayout mRefreshLayout;
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,6 +35,11 @@ public abstract class BaseFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this,view);
+        initializeViews(savedInstanceState);
+    }
+
+    private void initializeViews(Bundle savedInstanceState) {
+
     }
 }
